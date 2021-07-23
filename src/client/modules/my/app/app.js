6c6e54c58a70c6f963da8b92231a0434bdcc3161
Adrian Flores <adrian@imaginecrm.org>
2021-07-23 17:33:07 -0500

    Created app based on Trailhead LWC for OSS

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+	candidateId;
+	state = 'list';
+	handleNavigate(event) {
+		this.state = event.detail.state;
+		this.candidateId = event.detail.candidateId;
+	}
+	get isStateList() {
+		return this.state === 'list';
+	}
+	get isStateDetails() {
+		return this.state === 'details';
+	}
+}
