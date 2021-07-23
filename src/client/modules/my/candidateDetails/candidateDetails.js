f0be6b5424a3048a41cbef9b646fce2708ff4ca8
Adrian Flores <adrian@imaginecrm.org>
2021-07-23 17:34:29 -0500

    Created application based on Trailhead trail

---
+++ src/client/modules/my/candidateDetails/candidateDetails.js
@@ -0,0 +1,22 @@
+import { LightningElement, api } from 'lwc';
+import { getCandidate } from 'data/candidateService';
+export default class CandidateDetails extends LightningElement {
+    candidate;
+    @api
+    set candidateId(id) {
+        this._candidateId = id;
+        this.candidate = getCandidate(id);
+    }
+    get candidateId() {
+        return this._candidateId;
+    }
+    handleSessionsClick() {
+        console.log(JSON.stringify(this.candidate.career));
+        const navigateEvent = new CustomEvent('navigate', {
+            detail: {
+                state: 'list'
+            }
+        });
+        this.dispatchEvent(navigateEvent);
+    }
+}
